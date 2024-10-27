cdaa532942942f4d61914ab0487d89b70929f912
Mykola Pozniak <sheinnickolas@gmail.com>
2024-10-27 17:30:51 +0100

    fixed

--- create.sql
+++ create.sql
@@ -13,11 +13,10 @@ CREATE TABLE Teacher(
     First_name VARCHAR(30) NOT NULL CHECK (ISNUMERIC(First_name) = 0),
     Family_name VARCHAR(30) NOT NULL CHECK (ISNUMERIC(Family_name) = 0),
     Phone_number VARCHAR(11) NOT NULL CHECK (Phone_number LIKE '[0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9]'),
-    Subject_ VARCHAR(50) NOT NULL CHECK (ISNUMERIC(Subject_)=0)
 );
 
 CREATE TABLE Subjects(
-    SubjectID VARCHAR(3) PRIMARY KEY NOT NULL CHECK (SubjectID LIKE '[0-9][0-9][0-9]'),
+    SubjectID VARCHAR(3) NOT NULL CHECK (SubjectID LIKE '[0-9][0-9][0-9]'),
     Subject_name VARCHAR(30) NOT NULL CHECK (ISNUMERIC(Subject_name) = 0),
     Main_teacher VARCHAR(7) NOT NULL CHECK (Main_teacher LIKE 't[0-9][0-9][0-9][0-9][0-9][0-9]'),
     Year_of_actualization INT NOT NULL CHECK(Year_of_actualization>1800),
@@ -25,6 +24,7 @@ CREATE TABLE Subjects(
     Hours_ INT CHECK (Hours_ BETWEEN 0 AND 300),
     Year_ INT NOT NULL CHECK(Year_>1800),
     FOREIGN KEY (Main_teacher) REFERENCES Teacher(TeacherID) ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY(SubjectID,Year_)
 );
 
 CREATE TABLE Classes(
@@ -35,7 +35,8 @@ CREATE TABLE Classes(
     Type_of_class VARCHAR(30) NOT NULL CHECK (Type_of_class IN ('Wyklad', 'Laboratorium', 'Cwiczenia')),
     FOREIGN KEY (StudentID) REFERENCES Student(StudentID) ON DELETE NO ACTION ON UPDATE NO ACTION,
     FOREIGN KEY (TeacherID) REFERENCES Teacher(TeacherID) ON DELETE NO ACTION ON UPDATE NO ACTION,
-    FOREIGN KEY (SubjectID) REFERENCES Subjects(SubjectID) ON DELETE NO ACTION ON UPDATE NO ACTION
+    FOREIGN KEY (SubjectID) REFERENCES Subjects(SubjectID) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    PRIMARY KEY(StudentID,TeacherID,SubjectID,Type_of_class)
 );
 
 CREATE TABLE Reviews(
@@ -57,7 +58,9 @@ CREATE TABLE Funding(
     Partner_name VARCHAR(50) NOT NULL CHECK (ISNUMERIC(Partner_name) = 0),
     Grant_amount DECIMAL NOT NULL CHECK (Grant_amount >0),
     Date_ DATE NOT NULL,
-    Purporse VARCHAR(1000) CHECK(ISNUMERIC(Purporse)=0)
+    Purporse VARCHAR(3) NOT NULL CHECK (SubjectID LIKE '[0-9][0-9][0-9]'),
+    Commentary VARCHAR(1000) CHECK(ISNUMERIC(Commentary)=0),
+    FOREIGN KEY(Purporse) REFERENCES Subjects(SubjectID) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
 
@@ -82,6 +85,7 @@ CREATE TABLE Exam_grades(
     Grade INT NOT NULL CHECK (Grade BETWEEN 0 AND 100),
     Date_ DATE NOT NULL,
     FOREIGN KEY (StudentID) REFERENCES Student(StudentID) ON DELETE NO ACTION ON UPDATE NO ACTION,
-    FOREIGN KEY (SubjectID) REFERENCES Subjects(SubjectID) ON DELETE NO ACTION ON UPDATE NO ACTION
+    FOREIGN KEY (SubjectID) REFERENCES Subjects(SubjectID) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    PRIMARY KEY(StudentID,SubjectID,Date_)
 );
 
